4f19b03c1d9f6a4099ed41c6a6f6fd37f3628d3c
richie <richie.kwon1@gmail.com>
2022-08-10 09:41:23 +0100

    shipping cart functions

--- JenFood/Models/ShoppingCart.cs
+++ JenFood/Models/ShoppingCart.cs
@@ -1,4 +1,10 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using JenKitchen.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualBasic;
 
 namespace JenFood.Models
@@ -14,8 +20,91 @@ namespace JenFood.Models
         public ShoppingCart(AppDbContext appDbContext)
         {
             _appDbContext = appDbContext;
+
         }
-        
-        
+
+        public static ShoppingCart GetCart(IServiceProvider service)
+        {
+            ISession session = service.GetRequiredService<IHttpContextAccessor>()?.HttpContext.Session;
+
+            var context = service.GetService<AppDbContext>();
+
+            string cartId = session.GetString("CartId") ?? Guid.NewGuid().ToString();
+
+            session.SetString("CartId", cartId);
+
+            return new ShoppingCart(context) { ShoppingCartId = cartId };
+        }
+
+        public void AddToCart(Food food, int amount)
+        {
+            var shoppingCartItem = _appDbContext.ShoppingCartItems.SingleOrDefault(
+                s => s.Food.FoodId == food.FoodId && s.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem == null)
+            {
+                shoppingCartItem = new ShoppingCartItem
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Food = food,
+                    Amount = 1
+                };
+                _appDbContext.ShoppingCartItems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Amount++;
+            }
+
+            _appDbContext.SaveChanges();
+        }
+
+        public int RemoveFromCart(Food food)
+        {
+            var shoppingCartItem = _appDbContext.ShoppingCartItems.SingleOrDefault(
+                s => s.Food.FoodId == food.FoodId && s.ShoppingCartId == ShoppingCartId);
+
+            var localAmount = 0;
+
+            if (shoppingCartItem != null)
+            {
+                if (shoppingCartItem.Amount > 1)
+                {
+                    shoppingCartItem.Amount--;
+                    localAmount = shoppingCartItem.Amount;
+                }
+                else
+                {
+                    _appDbContext.ShoppingCartItems.Remove(shoppingCartItem);
+                }
+            }
+
+            _appDbContext.SaveChanges();
+            return localAmount;
+        }
+
+        public List<ShoppingCartItem> GetShoppingCartItems()
+        {
+            return ShoppingCartItems ??
+                   (ShoppingCartItems = _appDbContext.ShoppingCartItems.Where(
+                           c => c.ShoppingCartId == ShoppingCartId)
+                       .Include(s => s.Food)
+                       .ToList()
+                   );
+        }
+
+        public void ClearCart()
+        {
+            var cartItems = _appDbContext.ShoppingCartItems.Where(cart => cart.ShoppingCartId == ShoppingCartId);
+            _appDbContext.ShoppingCartItems.RemoveRange(cartItems);
+            _appDbContext.SaveChanges();
+        }
+
+        public decimal GetShoppingCartTotal()
+        {
+            var total = _appDbContext.ShoppingCartItems.Where(c => c.ShoppingCartId == ShoppingCartId)
+                .Select(c => c.Food.Price * c.Amount).Sum();
+            return total;
+        }
     }
 }
